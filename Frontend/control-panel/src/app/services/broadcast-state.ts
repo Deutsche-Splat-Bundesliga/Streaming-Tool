804d1aaeca9901535b1a5c9a57704b5491ec7b72
Raphi <289695277+BucketRaphi@users.noreply.github.com>
2026-08-30 12:50:04 +0200

    Remove debug console log

--- Frontend/control-panel/src/app/services/broadcast-state.ts
+++ Frontend/control-panel/src/app/services/broadcast-state.ts
@@ -71,7 +71,6 @@ export class BroadcastStateService {
 
         this.availableMaps.set(availableMaps);
         this.availableModes.set(availableModes);
-        console.log(availableModes);
       });
     });
 
