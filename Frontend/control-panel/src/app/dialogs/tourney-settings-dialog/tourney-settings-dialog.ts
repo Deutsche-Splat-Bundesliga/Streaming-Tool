3c75df2a4a2a4ac331046bb8bb4b3df324e409d1
Raphi <289695277+BucketRaphi@users.noreply.github.com>
2026-09-13 16:29:23 +0200

    Add first basic implementation of import and export for set data into json file

--- Frontend/control-panel/src/app/dialogs/tourney-settings-dialog/tourney-settings-dialog.ts
+++ Frontend/control-panel/src/app/dialogs/tourney-settings-dialog/tourney-settings-dialog.ts
@@ -1,9 +1,12 @@
-import { Component, inject, WritableSignal } from '@angular/core';
+import { Component, inject, OnDestroy, WritableSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 import { BroadcastStateService } from '../../services/broadcast-state';
 import { BroadcastState } from '../../models/broadcast-state';
 import { TranslocoDirective } from '@jsverse/transloco';
+import { formatDate } from '@angular/common';
+import { LogService } from '../../services/log';
+import { LogScope } from '../../models/log-scope';
 
 @Component({
   selector: 'app-tourney-settings-dialog',
@@ -11,7 +14,17 @@ import { TranslocoDirective } from '@jsverse/transloco';
   templateUrl: './tourney-settings-dialog.html',
   styleUrl: './tourney-settings-dialog.scss',
 })
-export class TourneySettingsDialog {
+export class TourneySettingsDialog implements OnDestroy {
+  /**
+   * Local logger instance for edit card operations.
+   */
+  private readonly _log: LogService = inject(LogService);
+
+  /**
+   * Logging scope for this component lifecycle and actions.
+   */
+  private readonly _scope: LogScope = this._log.beginScope('TourneySettingsDialog');
+
   /**
    * Service that manages broadcast state and division data.
    */
@@ -21,4 +34,88 @@ export class TourneySettingsDialog {
    * Writable signal representing the current broadcast state.
    */
   state: WritableSignal<BroadcastState> = this.stateService.state;
+
+  /**
+   * Export set data in JSON format, and download it to the drive of the user as a JSON file
+   */
+  async exportSetData(): Promise<void> {
+    const setData = {
+      tournamentName: this.state().tournamentName,
+      bracketName: this.state().bracketName,
+      teamAlpha: this.state().teamAlphaName,
+      teamBravo: this.state().teamBravoName,
+      division: this.state().division,
+      week: this.state().week,
+      season: this.state().season,
+      isLeague: this.state().isLeague,
+      maps: this.state().maps,
+    };
+    const blob = new Blob([JSON.stringify(setData, null, 2)], { type: 'application/json' });
+    const formattedDate = formatDate(new Date(), 'yyyyMMdd', 'en');
+    const fileName = `set-data-${formattedDate}.json`;
+
+    try {
+      // We are reaching fast inverse square root levels of cursed with this one
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const saveFileHandle = await (window as any).showSaveFilePicker({
+        suggestedName: fileName,
+        types: [
+          {
+            accept: {
+              'application/json': ['.json'],
+            },
+          },
+        ],
+      });
+      const saveFileWriter =
+        (await saveFileHandle.createWritable()) as FileSystemWritableFileStream;
+      await saveFileWriter.write(blob);
+      await saveFileWriter.close();
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      if (error.name === 'AbortError') {
+        return;
+      }
+
+      this._log.error('An error occured during the import of the set data JSON file!', error);
+    }
+  }
+
+  /**
+   * Import set data from a JSON file that sits on the drive of the user
+   */
+  async importSetData(): Promise<void> {
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const [setDataFile] = await (window as any).showOpenFilePicker({
+        types: [
+          {
+            accept: {
+              'application/json': ['.json'],
+            },
+          },
+        ],
+        multiple: false,
+      });
+
+      const file = await setDataFile.getFile();
+      const setData = JSON.parse(await file.text()) as Partial<BroadcastState>;
+      this.stateService.update(setData);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      if (error.name === 'AbortError') {
+        return;
+      }
+
+      this._log.error('An error occured during the import of the set data JSON file!', error);
+    }
+  }
+
+  /**
+   * Angular lifecycle hook called when the component is destroyed.
+   */
+  ngOnDestroy(): void {
+    this._log.trace('TourneySettingsDialog destroyed');
+    this._scope.dispose();
+  }
 }
