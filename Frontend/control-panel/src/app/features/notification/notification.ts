8009ab87afb4faac32cf1df97e7e46e744548b42
Raphi <289695277+BucketRaphi@users.noreply.github.com>
2026-09-22 22:33:34 +0200

    Implement slide in out animations and timer animations for notification

--- Frontend/control-panel/src/app/features/notification/notification.ts
+++ Frontend/control-panel/src/app/features/notification/notification.ts
@@ -1,6 +1,16 @@
-import { afterNextRender, Component, Input, OnDestroy } from '@angular/core';
+import {
+  afterNextRender,
+  Component,
+  ElementRef,
+  inject,
+  Input,
+  OnDestroy,
+  ViewContainerRef,
+  ViewChild,
+} from '@angular/core';
 import { NotificationType } from '../../enums/notification-type';
 import { NgClass } from '@angular/common';
+import { NotificationManager } from '../../services/notification-manager';
 
 @Component({
   imports: [NgClass],
@@ -9,11 +19,30 @@ import { NgClass } from '@angular/common';
   templateUrl: './notification.html',
 })
 export class Notification implements OnDestroy {
+  constructor(private readonly _elemRef: ViewContainerRef) {}
+
+  @Input({ required: true }) id: string = '';
   @Input({ required: true }) type: NotificationType = NotificationType.Info;
-
   @Input({ required: true }) text: string = '';
-
   @Input({ required: true }) duration: number = 5000;
+
+  @ViewChild('ntfTimer') private _ntfTimerElem?: ElementRef;
+
+  private readonly _notificationManager: NotificationManager = inject(NotificationManager);
+
+  private _timerAnimation?: Animation;
+
+  private readonly _slideInAnimKeyFrames: Keyframe[] = [
+    { opacity: 0, right: 'calc((var(--notification-width) + 2rem) * -1)' },
+    { opacity: 1, right: '0' },
+  ];
+  private readonly _slideInAnimOptions: KeyframeAnimationOptions = {
+    easing: 'ease-out',
+    fill: 'forwards',
+    duration: 500,
+  };
+
+  private readonly _timerAnimKeyFrames: Keyframe[] = [{ width: '100%' }, { width: '0' }];
 
   private _notificationTypeClasses: Map<NotificationType, string> = new Map([
     [NotificationType.Error, 'type--error'],
@@ -23,12 +52,12 @@ export class Notification implements OnDestroy {
   ]);
 
   private _onRenderEffect = afterNextRender(() => {
-    /*const notificationTypeClass = this._notificationTypeClasses.get(this.type);
-    if(!notificationTypeClass) {
-      throw new Error('Invalid NotificationType supplied when trying to create new notification!');
-    }*/
+    const hostElement = this._elemRef.element.nativeElement as HTMLElement;
+    const animation = hostElement.animate(this._slideInAnimKeyFrames, this._slideInAnimOptions);
 
-    console.log(this);
+    animation.onfinish = () => {
+      this.startTimer();
+    };
   });
 
   get notificationTypeClass(): string {
@@ -39,6 +68,44 @@ export class Notification implements OnDestroy {
 
     return notificationTypeClass;
   }
+
+  private startTimer(): void {
+    if (!this._ntfTimerElem) {
+      return;
+    }
+    const timerElem = this._ntfTimerElem.nativeElement as HTMLElement;
+
+    const timerAnimKeyframes: Keyframe[] = [{ width: '100%' }, { width: '0' }];
+    const timerAnimOptions: KeyframeAnimationOptions = {
+      duration: this.duration,
+      easing: 'linear',
+      fill: 'forwards',
+    };
+
+    const animation = timerElem.animate(timerAnimKeyframes, timerAnimOptions);
+    animation.onfinish = () => {
+      this.dismissNotification();
+    };
+  }
+
+  private dismissNotification(): void {
+    this._timerAnimation?.pause();
+
+    const hostElement = this._elemRef.element.nativeElement as HTMLElement;
+    const animation = hostElement.animate(
+      this._slideInAnimKeyFrames.reverse(),
+      this._slideInAnimOptions,
+    );
+    animation.onfinish = () => {
+      this._notificationManager.deleteNotification(this.id);
+    };
+  }
+
+  dismissBtnClick = (event: Event) => {
+    const button = event.target as HTMLElement;
+    button.toggleAttribute('disabled', true);
+    this.dismissNotification();
+  };
 
   ngOnDestroy(): void {
     this._onRenderEffect.destroy();
