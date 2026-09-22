06a02c422d9307c04d2c46f555e6e526b1b35171
Raphi <289695277+BucketRaphi@users.noreply.github.com>
2026-09-22 20:33:09 +0200

    Implement styles for notifications

--- Frontend/control-panel/src/app/features/notification/notification.ts
+++ Frontend/control-panel/src/app/features/notification/notification.ts
@@ -1,9 +1,46 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, Input, OnDestroy } from '@angular/core';
+import { NotificationType } from '../../enums/notification-type';
+import { NgClass } from '@angular/common';
 
 @Component({
-  imports: [],
+  imports: [NgClass],
   selector: 'app-notification',
   styleUrl: './notification.scss',
   templateUrl: './notification.html',
 })
-export class Notification {}
+export class Notification implements OnDestroy {
+  @Input({ required: true }) type: NotificationType = NotificationType.Info;
+
+  @Input({ required: true }) text: string = '';
+
+  @Input({ required: true }) duration: number = 5000;
+
+  private _notificationTypeClasses: Map<NotificationType, string> = new Map([
+    [NotificationType.Error, 'type--error'],
+    [NotificationType.Warning, 'type--warning'],
+    [NotificationType.Success, 'type--success'],
+    [NotificationType.Info, 'type--info'],
+  ]);
+
+  private _onRenderEffect = afterNextRender(() => {
+    /*const notificationTypeClass = this._notificationTypeClasses.get(this.type);
+    if(!notificationTypeClass) {
+      throw new Error('Invalid NotificationType supplied when trying to create new notification!');
+    }*/
+
+    console.log(this);
+  });
+
+  get notificationTypeClass(): string {
+    const notificationTypeClass = this._notificationTypeClasses.get(this.type);
+    if (!notificationTypeClass) {
+      throw new Error('Invalid NotificationType supplied when trying to create new notification!');
+    }
+
+    return notificationTypeClass;
+  }
+
+  ngOnDestroy(): void {
+    this._onRenderEffect.destroy();
+  }
+}
